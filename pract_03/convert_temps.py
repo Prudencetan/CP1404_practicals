b8fbc7a4bc800dd675d9256c420272eb6b104b0f
Prudencetan <prudence.tan@my.jcu.edu.au>
2020-08-05 02:08:29 +0800

    Refactor conversions to functions

--- pract_03/convert_temps.py
+++ pract_03/convert_temps.py
@@ -1,11 +1,29 @@
-from random import randint
+import random
 
 def main():
-    temps_file = open("temps_input.txt",'w')
+    input_file = open("temps_input.txt",'w')
+    output_file = open("temps_output.txt",'w')
+
+    for i in range(15):
+        print(random.uniform(-200.0,200.0),file=input_file)
+
+    input_file = open("temps_input.txt",'r')
+    temps_in_fahrenheit = input_file.readlines()
+
+    for line in temps_in_fahrenheit:
+        temperature = float(line)
+        print(fahrenheit_to_celsius(temperature),file=output_file)
+    input_file.close()
+    output_file.close()
 
 
-def fahrenheit_to_celsius(f,c):
+def fahrenheit_to_celsius(f):
+    c = 5 / 9 * (f - 32)
+    return c
+
+def celsius_to_fahrenheit(c):
     f = c * 9.0 / 5 + 32
+    return f
 
-def celsius_to_fahrenheit(c,f):
-    c = f / (9.0 / 5 + 32)
+if __name__ == '__main__':
+    main()
