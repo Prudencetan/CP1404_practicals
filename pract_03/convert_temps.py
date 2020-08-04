af0d1774889ec7193727839ccfba3ff420355399
Prudencetan <prudence.tan@my.jcu.edu.au>
2020-08-05 01:34:51 +0800

    Create file of temperatures

---
+++ pract_03/convert_temps.py
@@ -0,0 +1,11 @@
+from random import randint
+
+def main():
+    temps_file = open("temps_input.txt",'w')
+
+
+def fahrenheit_to_celsius(f,c):
+    f = c * 9.0 / 5 + 32
+
+def celsius_to_fahrenheit(c,f):
+    c = f / (9.0 / 5 + 32)
